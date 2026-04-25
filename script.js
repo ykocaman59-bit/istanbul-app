91b8b9e9cd975faaf9f455dabf7f52f94bf460c0
ykocaman59-bit <ykocaman59@gmail.com>
2026-04-25 15:28:44 +0300

    Initialize map and add marker for Istanbul

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function mesaj() {
   alert("İstanbul'u keşfetmeye hazır mısın?");
 }
+
+var map = L.map('map').setView([41.0082, 28.9784], 11);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+L.marker([41.0082, 28.9784]).addTo(map)
+  .bindPopup('İstanbul Merkez')
+  .openPopup();
