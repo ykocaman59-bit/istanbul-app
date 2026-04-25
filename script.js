03bd91422678473772143b4ff1e8bcb6cf259282
ykocaman59-bit <ykocaman59@gmail.com>
2026-04-25 16:46:25 +0300

    Refactor map markers to use dynamic data

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ function mesaj() {
   alert("İstanbul'u keşfetmeye hazır mısın?");
 }
 
-// Harita oluştur
+// 📍 VERİ SİSTEMİ
+const yerler = [
+  {
+    isim: "Ayasofya",
+    aciklama: "Tarihi ve büyüleyici yapı",
+    lat: 41.0055,
+    lng: 28.9768
+  },
+  {
+    isim: "Taksim Meydanı",
+    aciklama: "İstanbul’un kalbi",
+    lat: 41.0369,
+    lng: 28.9862
+  },
+  {
+    isim: "Galata Kulesi",
+    aciklama: "Manzarası efsane",
+    lat: 41.0256,
+    lng: 28.9744
+  }
+];
+
+// 🗺️ HARİTA
 var map = L.map('map').setView([41.0082, 28.9784], 11);
 
-// Harita katmanı
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// Noktalar
-L.marker([41.0082, 28.9784]).addTo(map)
-  .bindPopup('İstanbul Merkez')
-  .openPopup();
-
-L.marker([41.0369, 28.9862]).addTo(map)
-  .bindPopup('Taksim Meydanı');
-
-L.marker([41.0055, 28.9768]).addTo(map)
-  .bindPopup('Ayasofya');
+// 📌 OTOMATİK NOKTA EKLEME
+yerler.forEach(yer => {
+  L.marker([yer.lat, yer.lng]).addTo(map)
+    .bindPopup(`<b>${yer.isim}</b><br>${yer.aciklama}`);
+});
